ed9b6dfddf5f04cf57ff300e45796dae039b1c08
Tincho04 <mach_490@hotmail.com>
2021-03-18 21:07:09 -0300

    Primer Commit para Heroku

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { stringify } from '@angular/compiler/src/util';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Martín Silva';
+  sum = '';
+  prom = '';
+  edad1 = 'Edad1';
+  edad2 = 'Edad2';
+
+  CambiarTitulo()
+  {
+    this.title = "otro titulo";
+  }
+  Calcular()
+  {
+    var intEdad1 = parseInt(this.edad1);
+    var intEdad2 = parseInt(this.edad2);
+    var total = intEdad1 + intEdad2;
+    var promedio = total/2;
+
+    this.sum = 'Total de la suma: ' + total.toString();
+    this.prom = 'Promedio :' + promedio.toString();
+  }
+  Limpiar()
+  {
+    this.edad1 = "";
+    this.edad2 = "";
+  }
+}
